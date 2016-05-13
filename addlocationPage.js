ee4a77d3d678f537ab97f52339351453f04b3e73
tarad1 <tarad1@student.monash.edu>
2016-05-13 19:38:54 +1000

    Update addlocationPage.js

--- addlocationPage.js
+++ addlocationPage.js
@@ -1,5 +1,7 @@
 // Code for the Add Location page.
-function = addLocation(latitude, longitude, nickname)
+
+//Code to add location to Local Storage
+function addLocation(latitude, longitude, nickname)
 {
     var locationObject = {
                 lat: latitude,
@@ -9,11 +11,33 @@ function = addLocation(latitude, longitude, nickname)
 
             if (typeof(Storage) !== "undefined")
                 {
-                    var locationAsJSON = JSON.stringify(locationObject) //stringifying locationObject to JSON String
-                    localStorage.setItem(APP_PREFIX, JSON.stringify(locationObject)); //storing JSON string to localstorage as key "APP_PREFIX"
+                    for (var i = 1; i < 30; i++)
+                        {
+                            var flag = false
+                            if (localStorage.getItem("location"+i) === null || localStorage.getItem("location"+i) === "")
+                                {
+                                    var locationAsJSON = JSON.stringify(locationObject) //stringifying locationObject to JSON String
+                                    localStorage.setItem("location"+i, JSON.stringify(locationObject)); //storing JSON string to localstorage as key "APP_PREFIX"
+                                    alert("Location " + nickname + " added to LocalStorage "+i);
+                                    flag = true
+                                }
+                            if (flag === true)
+                                {
+                                    break; //breaks out of loop so we dont fill all locations
+                                }                
+                        }
+                                        
                 }
             else
                 {
                    alert("Error: localStorage is not supported by current browser.");
                 }
+    
+}
+
+//Code to remove location from Local Storage
+function removeLocation(number, nickname)
+{
+    localStorage.setItem("location"+number, null)
+    alert("Removed " + nickname + " (location " + number + ") from LocalStorage")
 }
