f34b7cb71a23dd7e181b7a975efee98019f7138f
tarad1 <tarad1@student.monash.edu>
2016-05-16 22:46:41 +1000

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -5,8 +5,18 @@
 
 function viewLocation(locationName)
 {
+    /*
     // Save the desired location to local storage
     localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
     // And load the view location page.
     location.href = 'viewlocation.html';
+    */
+    
+    // locationName = index of location
+    // Save the desired location to local storage
+    var wantedLocation = JSON.parse(localStorage.getItem("location"+locationName))
+    localStorage.setItem("selectedLocation", JSON.stringify(wantedLocation))
+    // And load the view location page.
+    location.href = 'viewlocation.html';
+    
 }
